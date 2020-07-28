0cbd1ec2ca5c4772490d5532b0d6d1cc79df1065
koumake <koumake422@gmail.com>
2020-07-28 11:44:57 +0900

    ユーザー認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sing_up, keys: [:firstname, :lastname, :firstname_alias, :lastname_alias, :nickname, :birthday])
+  end
 end

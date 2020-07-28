3a1a08fe80fb1346d6232ae5ec46cff4df322ece
koumake <koumake422@gmail.com>
2020-07-28 12:21:39 +0900

    userdevise完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :firstname, :lastname, :firstname_alias, :lastname_alias, :nickname, :birthday, presence: true
 end
